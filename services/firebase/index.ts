735167550ede5086555f0783e358de13b24c9dec
Wilson Wong <wilson.wong@gauntletai.com>
2025-02-05 18:18:41 -0600

    keys to .env

--- services/firebase/index.ts
+++ services/firebase/index.ts
@@ -20,12 +20,12 @@ import { Video } from '../../types/video';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyDfr1xAbYeMtiKWyzlgmct3HMSk3qOwg",
-  authDomain: "tiktok-3142f.firebaseapp.com",
-  projectId: "tiktok-3142f",
-  storageBucket: "tiktok-3142f.firebasestorage.app",
-  messagingSenderId: "733898101817",
-  appId: "1:733898101817:web:2f28459bd9f0fb726d69e8"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
